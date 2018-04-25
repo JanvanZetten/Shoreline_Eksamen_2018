f355abc55d32da8a6fabc1fb4c154b6d1627a06f
JanvanZetten <33052378+JanvanZetten@users.noreply.github.com>
2018-04-25 09:14:16 +0200

    gui refaktoring in packages

--- Shoreline_Exam_2018/src/shoreline_exam_2018/gui/controller/FXMLDocumentController.java
+++ Shoreline_Exam_2018/src/shoreline_exam_2018/gui/controller/FXMLDocumentController.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package shoreline_exam_2018;
+package shoreline_exam_2018.gui.controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
