bfed969bc1f0dddfd56320cb1dbe2a8d6720f848
Applemelon <32298607+Applemelon@users.noreply.github.com>
2018-04-25 12:23:08 +0200

    Created file chooser

--- Shoreline_Exam_2018/src/shoreline_exam_2018/gui/model/MainModel.java
+++ Shoreline_Exam_2018/src/shoreline_exam_2018/gui/model/MainModel.java
@@ -5,10 +5,39 @@
  */
 package shoreline_exam_2018.gui.model;
 
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import javafx.stage.FileChooser;
+import javafx.stage.FileChooser.ExtensionFilter;
+
 /**
  *
  * @author janvanzetten
  */
 public class MainModel {
     
+    private File selectedFile;
+    private Path to;
+    private Path from;
+
+    public void chooseFile() {
+//        ExtensionFilter filter = new ExtensionFilter(".xslx");
+        FileChooser fc = new FileChooser();
+        
+//        fc.getExtensionFilters().add(filter);
+        String currentDir = System.getProperty("user.dir") + File.separator;
+        
+        File dir = new File(currentDir);
+        fc.setInitialDirectory(dir);
+        fc.setTitle("Attach a file");
+        
+        selectedFile = fc.showOpenDialog(null);
+        System.out.println(selectedFile);
+        
+//        if (selectedFile != null) {
+//            from = Paths.get(selectedFile.toURI());
+//            to = Paths.get(dir + "/music/" + selectedFile.getName());
+//    }
+    }
 }
