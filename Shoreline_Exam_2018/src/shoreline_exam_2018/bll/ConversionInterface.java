c581c42d5de8dd15ba8928002abca420ec311ffb
Applemelon <32298607+Applemelon@users.noreply.github.com>
2018-04-25 12:05:00 +0200

    ConversionInterface and ConvertXSLM classes created

---
+++ Shoreline_Exam_2018/src/shoreline_exam_2018/bll/ConversionInterface.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package shoreline_exam_2018.bll;
+
+import java.io.File;
+import shoreline_exam_2018.be.Profile;
+
+/**
+ *
+ * @author alexl
+ */
+public interface ConversionInterface {
+    
+    /**
+     * Converts the file.
+     * @param selectedProfile
+     * @param selectedFile 
+     */
+    public void convertFile(Profile selectedProfile, File selectedFile);
+    
+}
---
+++ Shoreline_Exam_2018/src/shoreline_exam_2018/bll/ConvertXSLM.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package shoreline_exam_2018.bll;
+
+import java.io.File;
+import shoreline_exam_2018.be.Profile;
+
+/**
+ *
+ * @author alexl
+ */
+public class ConvertXSLM implements ConversionInterface {
+
+    @Override
+    public void convertFile(Profile selectedProfile, File selectedFile) {
+        //Do conversion
+    }
+    
+}
