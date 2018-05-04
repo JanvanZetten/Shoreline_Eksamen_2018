cf1e99b42094af0040bcc648b4abb580279d639f
Asbjørn Mansa Jensen <asbj0378@easv365.dk>
2018-05-04 11:26:59 +0200

    No division by zero

--- Shoreline_Exam_2018/src/shoreline_exam_2018/bll/ConvertXlsxToJson.java
+++ Shoreline_Exam_2018/src/shoreline_exam_2018/bll/ConvertXlsxToJson.java
@@ -29,108 +29,136 @@ import shoreline_exam_2018.be.output.structure.StructEntityInterface;
  *
  * @author Jan
  */
-public class ConvertXlsxToJson implements ConversionInterface {
+public class ConvertXlsxToJson implements ConversionInterface
+{
 
     InputFileReader reader;
 
-    
     /**
-     * 
-     * Convert inputfile acording to the given profile and write it to the outputfile
+     *
+     * Convert inputfile acording to the given profile and write it to the
+     * outputfile
      * @throws shoreline_exam_2018.bll.BLLExeption
      */
     @Override
-    public void convertFile(Profile selectedProfile, Path inputFile, Path outputFile, MutableBoolean isCanceld, MutableBoolean isOperating, DoubleProperty progress) throws BLLExeption {
+    public void convertFile(Profile selectedProfile, Path inputFile, Path outputFile, MutableBoolean isCanceld, MutableBoolean isOperating, DoubleProperty progress) throws BLLExeption
+    {
         reader = new XLSX_horisontal_Reader(inputFile.toString());
-        
+
         StructEntityObject structure = selectedProfile.getStructure();
 
         List<OutputPair> outputObjects = new ArrayList<>();
 
-        try {
+        try
+        {
             int totalRows = reader.numberOfRows();
             int currentrow = 0;
-            while (reader.hasNext()) {
-                
-                progress.setValue(totalRows/currentrow*100); // sets the percent of done
-                
-                if (isCanceld.getValue()){
+            while (reader.hasNext())
+            {
+                currentrow++;
+
+                progress.setValue(totalRows / currentrow * 100); // sets the percent of done
+
+                if (isCanceld.getValue())
+                {
                     return;
                 }
-                while(!isOperating.getValue()){
+                while (!isOperating.getValue())
+                {
                     Thread.sleep(1000);
                 }
-                
+
                 Row nextRow = reader.getNextRow();
 
                 OutputPair outputpair = new JsonPairJson("", mapRowToOutputpairWithEntityCollection(structure, nextRow));
 
                 outputObjects.add(outputpair);
-                currentrow ++;
             }
-            
+
             System.out.println("Done converting --- written from ConvertXlsxToJson line 60");
-            
+
             new JsonDAO().createFile(outputObjects, outputFile);
-            
-        } catch (DALException ex) {
+
+        }
+        catch (DALException ex)
+        {
             throw new BLLExeption(ex.getMessage(), ex.getCause());
-        } catch (InterruptedException ex) {
+        }
+        catch (InterruptedException ex)
+        {
             Logger.getLogger(ConvertXlsxToJson.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
 
     /**
-     * get a List of outputpairs with the data from the row and the structure and fields from the structObject
-     * @param structObject the object describing which data should convert to what
+     * get a List of outputpairs with the data from the row and the structure
+     * and fields from the structObject
+     * @param structObject the object describing which data should convert to
+     * what
      * @param row the row from which to load the data
      * @return a list of outputpairs
-     * @throws BLLExeption if the stuctObject has a structEntry which is not supported. The supported are: StructEntryArray, StructEntryDate, 
-     * StructEntryDouble, StructEntryInteger, StructEntryObject and StructEntryString.
+     * @throws BLLExeption if the stuctObject has a structEntry which is not
+     * supported. The supported are: StructEntryArray, StructEntryDate,
+     * StructEntryDouble, StructEntryInteger, StructEntryObject and
+     * StructEntryString.
      */
-    private List<OutputPair> mapRowToOutputpairWithEntityCollection(CollectionEntity structObject, Row row) throws BLLExeption {
+    private List<OutputPair> mapRowToOutputpairWithEntityCollection(CollectionEntity structObject, Row row) throws BLLExeption
+    {
         List<StructEntityInterface> collection = structObject.getCollection();
-        
-        List<OutputPair> output = new ArrayList<>();
-        
-        for (StructEntityInterface structEntry : collection) {
-            
-            //check what object this structE
 
-            if (structEntry instanceof StructEntityArray) {
-                
-                List<OutputPair> jsonArray = mapRowToOutputpairWithEntityCollection((StructEntityArray)structEntry, row);
+        List<OutputPair> output = new ArrayList<>();
+
+        for (StructEntityInterface structEntry : collection)
+        {
+            System.out.println(structEntry);
+            //check what object this structE
+            if (structEntry instanceof StructEntityArray)
+            {
+
+                List<OutputPair> jsonArray = mapRowToOutputpairWithEntityCollection((StructEntityArray) structEntry, row);
                 output.add(new JsonPairArray(structEntry.getColumnName(), jsonArray));
-                
-            } else if (structEntry instanceof StructEntityDate) {
-                
-                output.add(new JsonPairDate(structEntry.getColumnName(), 
+
+            }
+            else if (structEntry instanceof StructEntityDate)
+            {
+
+                output.add(new JsonPairDate(structEntry.getColumnName(),
                         row.getCell(((StructEntityDate) structEntry).getInputIndex()).getDateCellValue()));
 
-            } else if (structEntry instanceof StructEntityDouble) {
-                
+            }
+            else if (structEntry instanceof StructEntityDouble)
+            {
+
                 output.add(new JsonPairDouble(structEntry.getColumnName(),
                         row.getCell(((StructEntityDouble) structEntry).getInputIndex()).getNumericCellValue()));
-                
-            } else if (structEntry instanceof StructEntityInteger) {
+
+            }
+            else if (structEntry instanceof StructEntityInteger)
+            {
                 Double asDouble = row.getCell(((StructEntityInteger) structEntry).getInputIndex()).getNumericCellValue();
                 output.add(new JsonPairInteger(structEntry.getColumnName(), asDouble.intValue()));
 
-            } else if (structEntry instanceof StructEntityObject) {
-                List<OutputPair> jsonObject = mapRowToOutputpairWithEntityCollection((StructEntityObject)structEntry, row);
+            }
+            else if (structEntry instanceof StructEntityObject)
+            {
+                List<OutputPair> jsonObject = mapRowToOutputpairWithEntityCollection((StructEntityObject) structEntry, row);
                 output.add(new JsonPairJson(structEntry.getColumnName(), jsonObject));
-                
-            } else if (structEntry instanceof StructEntityString) {
-                
-                output.add(new JsonPairString(structEntry.getColumnName(), 
+
+            }
+            else if (structEntry instanceof StructEntityString)
+            {
+
+                output.add(new JsonPairString(structEntry.getColumnName(),
                         row.getCell(((StructEntityString) structEntry).getInputIndex()).getStringCellValue()));
 
-            } else {
+            }
+            else
+            {
                 throw new BLLExeption("The struct entry type is not supported");
             }
 
         }
-        
+
         return output;
 
     }
