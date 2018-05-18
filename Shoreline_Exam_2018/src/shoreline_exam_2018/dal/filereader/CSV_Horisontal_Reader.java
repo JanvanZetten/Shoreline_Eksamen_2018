ce016ac0bcbbdef8896317c1578e33bf9f41f547
JanvanZetten <33052378+JanvanZetten@users.noreply.github.com>
2018-05-18 09:10:36 +0200

    CSV reader

--- Shoreline_Exam_2018/src/shoreline_exam_2018/dal/filereader/CSV_Horisontal_Reader.java
+++ Shoreline_Exam_2018/src/shoreline_exam_2018/dal/filereader/CSV_Horisontal_Reader.java
@@ -5,7 +5,19 @@
  */
 package shoreline_exam_2018.dal.filereader;
 
+import com.opencsv.CSVReader;
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import shoreline_exam_2018.be.InputObject;
 import shoreline_exam_2018.dal.DALException;
 
@@ -13,32 +25,145 @@ import shoreline_exam_2018.dal.DALException;
  *
  * @author janvanzetten
  */
-public class CSV_Horisontal_Reader implements Reader{
+public class CSV_Horisontal_Reader implements Reader {
 
-    public CSV_Horisontal_Reader(String FileName) {
-        
-        
-    }
+    CSVReader mainReader;
+    Iterator<String[]> iterator;
+    String fileName;
+    private static final long EXPIRATION_TIME = 10000; //in milliseconds
+    private long timeouttime = System.currentTimeMillis() + EXPIRATION_TIME;
+    private boolean open = false;
 
     
+    public CSV_Horisontal_Reader(String fileName) throws DALException {
+        try {
+            mainReader = new CSVReader(new FileReader(new File(fileName)));
+            iterator = mainReader.iterator();
+            iterator.next();// first line should be header
+            this.fileName = fileName;
+        } catch (FileNotFoundException ex) {
+            throw new DALException(ex.getMessage(), ex.getCause());
+        }
+
+    }
+
     @Override
     public List<String> getParameters() throws DALException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        try {
+            CSVReader csvReader = new CSVReader(new FileReader(new File(fileName)));
+
+            String[] readNext = csvReader.readNext();
+
+            return Arrays.asList(readNext);
+
+        } catch (FileNotFoundException ex) {
+            throw new DALException(ex.getMessage(), ex.getCause());
+        } catch (IOException ex) {
+            throw new DALException(ex.getMessage(), ex.getCause());
+        }
+
     }
 
     @Override
     public boolean hasNext() throws DALException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        timeouttime = System.currentTimeMillis() + EXPIRATION_TIME;
+        if (!open) {
+            mainReader = openStream();
+            makeTimeout();
+            open = true;
+        }
+        if (iterator.hasNext()) {
+            return true;
+        } else {
+            closeMainStream();
+            return false;
+        }
     }
 
     @Override
     public InputObject getNext() throws DALException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        timeouttime = System.currentTimeMillis() + EXPIRATION_TIME;
+        
+        if (!open) {
+            mainReader = openStream();
+
+            open = true;
+        }
+        
+        String[] row = iterator.next();
+
+        return InputObjectConverter.StringArrayToInputObject(row);
     }
 
     @Override
     public int numberOfRows() throws DALException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        InputStream is = null;
+        
+        try {
+            is = new BufferedInputStream(new FileInputStream(fileName));
+            byte[] c = new byte[1024];
+            int count = 0;
+            int readChars = 0;
+            boolean empty = true;
+            while ((readChars = is.read(c)) != -1) {
+                empty = false;
+                for (int i = 0; i < readChars; ++i) {
+                    if (c[i] == '\n') {
+                        ++count;
+                    }
+                }
+            }
+            return (count == 0 && !empty) ? 1 : count;
+        } catch (FileNotFoundException ex) {
+            throw new DALException(ex.getMessage(), ex.getCause());
+        } catch (IOException ex) {
+            throw new DALException(ex.getMessage(), ex.getCause());
+        } finally {
+            try {
+                is.close();
+            } catch (IOException ex) {
+                throw new DALException(ex.getMessage(), ex.getCause());
+            }
+        }
+
     }
-    
+
+    private CSVReader openStream() throws DALException {
+        try {
+            return new CSVReader(new FileReader(new File(fileName)));
+        } catch (FileNotFoundException ex) {
+           throw new DALException(ex.getMessage(), ex.getCause());
+        }
+    }
+
+    private void makeTimeout() {
+        Thread thread;
+        thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (System.currentTimeMillis() < timeouttime) {
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException ex) {
+                        Logger.getLogger(XLSX_horisontal_Reader_for_Big_Documents.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                }
+                try {
+                    closeMainStream();
+                } catch (DALException ex) {
+                    Logger.getLogger(XLSX_horisontal_Reader_for_Big_Documents.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        });
+
+        thread.start();
+    }
+
+    private void closeMainStream() throws DALException {
+        try {
+            mainReader.close();
+        } catch (IOException ex) {
+            throw new DALException(ex.getMessage(), ex.getCause());
+        }
+    }
 }
