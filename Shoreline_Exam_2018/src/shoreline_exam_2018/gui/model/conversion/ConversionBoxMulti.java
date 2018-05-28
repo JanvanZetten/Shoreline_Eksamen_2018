b455af0e6c1315cb08a8313026b500d3a89fd6b0
Asbjørn Mansa Jensen <asbj0378@easv365.dk>
2018-05-28 11:48:22 +0200

    Conversion: Fixed graphic mess when errors on more conversions occur at once

--- Shoreline_Exam_2018/src/shoreline_exam_2018/gui/model/conversion/ConversionBoxMulti.java
+++ Shoreline_Exam_2018/src/shoreline_exam_2018/gui/model/conversion/ConversionBoxMulti.java
@@ -10,62 +10,55 @@ import shoreline_exam_2018.be.Profile;
  *
  * @author Alex
  */
-public class ConversionBoxMulti extends TitledPane implements ConversionBoxInterface {
-    
+public class ConversionBoxMulti extends TitledPane implements ConversionBoxInterface
+{
+
     private final AnchorPane pane = new AnchorPane();
     private final ListView<ConversionBoxInterface> multiList;
     private final Profile selectedProfile;
     private ArrayList<ConversionBoxSingle> boxes;
-    
+
     // Default size of ConversionBoxSingle inside a Multi.
     private final int JOB_SIZE = 56;
     // Default border size on COnversionJobMulti.
     private double paneSize = 44;
-    
+
     /**
-     * Creates a ConversionJobMulti, which is the container of multiple 
+     * Creates a ConversionJobMulti, which is the container of multiple
      * ConversionJobSingles if a folder is selected to be converted from.
      * @param selectedProfile = The currently selected profile
      * @param list
      */
-    public ConversionBoxMulti(Profile selectedProfile, ListView<ConversionBoxInterface> list) {
+    public ConversionBoxMulti(Profile selectedProfile, ListView<ConversionBoxInterface> list)
+    {
         super();
-        
+
         this.multiList = list;
         this.selectedProfile = selectedProfile;
-        
-        pane.setBottomAnchor(multiList, 0.0);
-        pane.setTopAnchor(multiList, 0.0);
-        pane.setRightAnchor(multiList, 0.0);
-        pane.setLeftAnchor(multiList, 0.0);
+
+        AnchorPane.setBottomAnchor(multiList, 0.0);
+        AnchorPane.setTopAnchor(multiList, 0.0);
+        AnchorPane.setRightAnchor(multiList, 0.0);
+        AnchorPane.setLeftAnchor(multiList, 0.0);
         pane.getChildren().add(this.multiList);
-        
+
         this.multiList.setStyle("-fx-padding: 0px;");
-        
+
         this.setContent(pane);
-    }
-    
-    /**
-     * Sets up the pane and Array of boxes inside this object.
-     * @param jobs 
-     */
-    public void setupPane(ArrayList<ConversionBoxSingle> jobs) {
-        this.boxes = jobs;
-        
+
+        this.boxes = new ArrayList<>();
         pane.setMinHeight(0);
-        pane.setPrefHeight(paneSize(jobs));
-        
-        this.setText(jobs.size() + " files with the " + selectedProfile.getName() + " profile");
-        this.setPressed(true);
     }
-    
+
     /**
      * Retrieves the amount of boxes present in the Array and sets them.
      * @param jobs
-     * @return 
+     * @return
      */
-    private double paneSize(ArrayList<ConversionBoxSingle> jobs) {
-        for (ConversionBoxSingle job : jobs) {
+    private double paneSize(ArrayList<ConversionBoxSingle> jobs)
+    {
+        for (ConversionBoxSingle job : jobs)
+        {
             multiList.getItems().add(job);
             paneSize = paneSize + JOB_SIZE;
         }
@@ -74,44 +67,48 @@ public class ConversionBoxMulti extends TitledPane implements ConversionBoxInter
 
     /**
      * Gets the ListView.
-     * @return 
+     * @return
      */
-    public ListView<ConversionBoxInterface> getListJobs() {
+    public ListView<ConversionBoxInterface> getListJobs()
+    {
         return multiList;
     }
-    
+
     /**
      * Add box to the multi conversion box
-     * @param conversion 
+     * @param conversion
      */
-    public void addBox(ConversionBoxSingle conversion){
+    public void addBox(ConversionBoxSingle conversion)
+    {
         boxes.add(conversion);
-        
+
         this.setText(boxes.size() + " files with the " + selectedProfile.getName() + " profile");
-        
+
         paneSize = paneSize + JOB_SIZE;
-        
+
         pane.setPrefHeight(paneSize);
-        
+
         multiList.getItems().add(conversion);
     }
-    
+
     /**
      * Is called whenever a box has finished inside this object. Resizes the
      * AnchorPane dynamically as boxes are deleted from the list.
-     * @param box 
+     * @param box
      */
-    public void notifyDeletedJob(ConversionBoxSingle box) {
+    public void notifyDeletedJob(ConversionBoxSingle box)
+    {
         boxes.remove(box);
         this.setText(boxes.size() + " files with the " + selectedProfile.getName() + " profile");
-        
+
         paneSize = paneSize - JOB_SIZE;
-        
+
         pane.setPrefHeight(paneSize);
-        
+
     }
 
-    public Profile getProfile() {
+    public Profile getProfile()
+    {
         return selectedProfile;
     }
 }
